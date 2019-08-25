63ed7f6120c83cde04c854c569c54222503a00e7
a-poor <a-poor@users.noreply.github.com>
2019-08-25 12:36:42 -0400

    Updating description and fixing issue with convergence

--- sketch.js
+++ sketch.js
@@ -53,69 +53,70 @@ function setup() {
 }
 
 function draw() {
-  colorMode(RGB, 255);
-  background(0);
-  point_clusters = [];
-  for (let i = 0; i < k; i++) point_clusters.push([]);
-
-  // Iterate through the points
-  strokeWeight(5);
-  for (let p of points) {
-    // Find the mean they're closest to
-    let d = [];
-    let best_d;
-    let best_i;
-    for (let i = 0; i < means.length; i++) {
-      let mean_distance = p.dist(means[i]);
-      d.push(mean_distance);
-      if (best_d == undefined || mean_distance < best_d) {
-        best_d = mean_distance;
-        best_i = i;
-      }
-    }
-    // Draw them in that color
-    stroke(cluster_colors[best_i]);
-    point(p.x, p.y);
-    // Add them to the correct cluster array
-    point_clusters[best_i].push(p);
-  }
-
-  // Draw the mean points bigger
-  strokeWeight(10);
-  stroke(255);
-  for (let i = 0; i < means.length; i++) {
-    point(means[i].x,means[i].y);
-  }
-
-  // Find the new mean for each cluster
-  let new_means = [];
-  for (let i = 0; i < point_clusters.length; i++) {
-    new_means.push(createVector());
-    for (let p of point_clusters[i]) {
-      new_means[i].add(p);
-    }
-    new_means[i].div(point_clusters[i].length);
-  }
-  let means_changed = false;
-  for (let i = 0; i < means.length; i++) {
-    if (means[i].dist(new_means[i]) > convergence_tollerence) {
-      means_changed = true;
-      break;
-    }
-  }
-  if (means_changed) {
-    means = new_means;
-  } else {
-    console.log("Convergence reached. Stopping.");
-    convergence_reached = true;
-    noLoop();
-  }
   if (convergence_reached) {
     noFill();
     stroke(100,200,100);
     strokeWeight(10);
     rect(0,0,width,height);
   } else {
+    colorMode(RGB, 255);
+    background(0);
+    point_clusters = [];
+    for (let i = 0; i < k; i++) point_clusters.push([]);
+
+    // Iterate through the points
+    strokeWeight(5);
+    for (let p of points) {
+      // Find the mean they're closest to
+      let d = [];
+      let best_d;
+      let best_i;
+      for (let i = 0; i < means.length; i++) {
+        let mean_distance = p.dist(means[i]);
+        d.push(mean_distance);
+        if (best_d == undefined || mean_distance < best_d) {
+          best_d = mean_distance;
+          best_i = i;
+        }
+      }
+      // Draw them in that color
+      stroke(cluster_colors[best_i]);
+      point(p.x, p.y);
+      // Add them to the correct cluster array
+      point_clusters[best_i].push(p);
+    }
+
+    // Draw the mean points bigger
+    strokeWeight(10);
+    stroke(255);
+    for (let i = 0; i < means.length; i++) {
+      point(means[i].x,means[i].y);
+    }
+
+    // Find the new mean for each cluster
+    let new_means = [];
+    for (let i = 0; i < point_clusters.length; i++) {
+      new_means.push(createVector());
+      for (let p of point_clusters[i]) {
+        new_means[i].add(p);
+      }
+      new_means[i].div(point_clusters[i].length);
+    }
+    let means_changed = false;
+    for (let i = 0; i < means.length; i++) {
+      if (means[i].dist(new_means[i]) > convergence_tollerence) {
+        means_changed = true;
+        break;
+      }
+    }
+    if (means_changed) {
+      means = new_means;
+    } else {
+      console.log("Convergence reached. Stopping.");
+      convergence_reached = true;
+      noLoop();
+    }
+
     noFill();
     stroke(250,100,100);
     strokeWeight(10);
@@ -126,6 +127,7 @@ function draw() {
 
 function initialize() {
   // Create the points
+  loop();
   points = [];
   for (let i = 0; i < n_points; i++) {
     points.push(createVector(random(width), random(height)));
